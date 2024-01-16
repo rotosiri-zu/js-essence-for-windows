471d7bd6bcba0ea696d9d75503ce0975dff13b11
rotosiri-zu <hongjitufang25@gmail.com>
2024-01-16 21:56:22 +0900

    終了確認_bindとthis

--- javascript-essence-tutorial/050_関数とオブジェクト１/920_修了確認_bindとthis/end/main.js
+++ javascript-essence-tutorial/050_関数とオブジェクト１/920_修了確認_bindとthis/end/main.js
@@ -1,41 +1,41 @@
 const person = {
-    name: 'Tom',
-    bye() {
-        console.log('Bye ' + this.name);
-    },
-    hello: function (greeting) {
-        console.log(greeting + ' ' + this.name);
-        return greeting + ' ' + this.name;
-    },
-    /**
-     * 問題４：
-     * 1秒後に"hello Tom"
-     * と出力されるような、メソッドを
-     * personオブジェクトに追加してみてください。
-     * 
-     * 以下のように使用するものとします。
-     * `person.hello1s()` 
-     * -> 1秒後に"hello Tom"と出力
-     * 
-     * 3通りの方法で実装してみてください。
-     * １．bind
-     * ２．アロー関数
-     * ３．thisを一旦変数に代入
-     */
-    hello1s() {
-        // setTimeout(this.hello.bind(this, 'hello'), 1000);
+  name: 'Tom',
+  bye() {
+    console.log('Bye ' + this.name)
+  },
+  hello: function (greeting) {
+    console.log(greeting + ' ' + this.name)
+    return greeting + ' ' + this.name
+  },
+  /**
+   * 問題４：
+   * 1秒後に"hello Tom"
+   * と出力されるような、メソッドを
+   * personオブジェクトに追加してみてください。
+   *
+   * 以下のように使用するものとします。
+   * `person.hello1s()`
+   * -> 1秒後に"hello Tom"と出力
+   *
+   * 3通りの方法で実装してみてください。
+   * １．bind
+   * ２．アロー関数
+   * ３．thisを一旦変数に代入
+   */
+  hello1s() {
+    // setTimeout(this.hello.bind(this, 'hello'), 1000);
 
-        // setTimeout(() => {
-        //     this.hello('hello');
-        // }, 1000);
+    // setTimeout(() => {
+    // this.hello('hello');
+    // }, 1000)
 
-        const _this = this;
-        setTimeout(function() {
-            _this.hello('hello');
-        })
-    }
+    const _this = this
+    setTimeout(function () {
+      _this.hello('hello')
+    })
+  },
 }
-person.hello1s();
+person.hello1s()
 /**
  * 問題１：
  * 1秒後に"hello Tom"
