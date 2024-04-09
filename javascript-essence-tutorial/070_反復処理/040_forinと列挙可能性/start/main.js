8d42e1d7bc23af13faf33a39e1657e7d68b4251c
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-09 18:15:08 +0900

    forinと列挙可能性

--- javascript-essence-tutorial/070_反復処理/040_forinと列挙可能性/start/main.js
+++ javascript-essence-tutorial/070_反復処理/040_forinと列挙可能性/start/main.js
@@ -1,5 +1,27 @@
+const s = Symbol()
 const obj = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3'
+  prop1: 'value1',
+  prop2: 'value2',
+  prop3: 'value3',
+  [s]: 'value4',
+}
+
+Object.prototype.method = function () {}
+// Object.defineProperty(Object.prototype, 'method', {
+//   enumerable: false,
+// })
+
+// Object.defineProperty(obj, 'prop1', {
+//   enumerable: false,
+// })
+
+// const d = Object.getOwnPropertyDescriptor(object.prototype, 'method')
+const d = Object.getOwnPropertyDescriptor(object.prototype, 'hasOwnProperty')
+const e = Object.getOwnPropertyDescriptor(obj, s)
+console.log(e)
+
+for (let key in obj) {
+  // if (obj.hasOwnProperty(key)) {
+  console.log(key, obj[key])
+  // }
 }
