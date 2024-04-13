8c8111a9f796b168040deb9aaad4c86045e68d0d
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-13 16:05:25 +0900

    修了確認_PubSubパターン

--- javascript-essence-tutorial/070_反復処理/930_修了確認_PubSubパターン/start/main.js
+++ javascript-essence-tutorial/070_反復処理/930_修了確認_PubSubパターン/start/main.js
@@ -4,17 +4,17 @@
  */
 
 function customFn1() {
-	console.log('customFn1');
+  console.log('customFn1')
 }
 function customFn2() {
-	console.log('customFn2');
+  console.log('customFn2')
 }
 function customFn3() {
-	console.log('customFn3');
+  console.log('customFn3')
 }
 
-events.on('beforeInit', customFn1);
-events.on('beforeInit', customFn2);
-events.on('afterInit', customFn3);
+events.on('beforeInit', customFn1)
+events.on('beforeInit', customFn2)
+events.on('afterInit', customFn3)
 
-new MyLibrary();
+new MyLibrary()
