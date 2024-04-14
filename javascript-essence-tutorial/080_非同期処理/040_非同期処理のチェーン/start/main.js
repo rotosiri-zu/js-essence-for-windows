00baa6b5eec7e89d4c52abeabb69f6da899982c6
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-14 16:20:17 +0900

    非同期処理のチェーン

---
+++ javascript-essence-tutorial/080_非同期処理/040_非同期処理のチェーン/start/main.js
@@ -0,0 +1,13 @@
+function sleep(callback, val) {
+  setTimeout(function () {
+    console.log(val++)
+    callback(val)
+  }, 1000)
+}
+
+sleep(function (val) {
+  sleep(function (val) {}, val)
+  sleep(function (val) {}, val)
+  sleep(function (val) {}, val)
+  sleep(function (val) {}, val)
+}, 0)
