ff6974ede6553b3028274766e4ed4178897aadc3
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-19 16:18:52 +0900

    例外処理とエラー

--- javascript-essence-tutorial/080_非同期処理/120_例外処理とエラー/start/main.js
+++ javascript-essence-tutorial/080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,11 +1,47 @@
-
-
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
+// try {
+//   console.log('start')
+//   // throw new Error('error message')
+//   console.log('end')
+// } catch (e) {
+//   console.error(e)
+// } finally {
+//   console.log('bye')
 // }
 
-// fetchUsers();
+async function fetchUsers() {
+  const response = await fetch('users.json')
+  if (response.ok) {
+    const json = await response.json()
+    if (!json.length) {
+      throw new Error('no data found')
+    }
+    return json
+  }
+}
+
+class NoDataError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'NoDataError'
+  }
+}
+
+async function init() {
+  try {
+    const User = await fetchUsers()
+    for (const user of users) {
+      console.log(`I'm ${user.name}, ${user.age} years old`)
+    }
+  } catch (e) {
+    if (e instanceof NoDataError) {
+      console.log(e)
+    } else {
+      console.error('Oops, something went wrong')
+    }
+    console.error(e)
+  } finally {
+    console.log('bye')
+  }
+  console.log('end')
+}
+init()
