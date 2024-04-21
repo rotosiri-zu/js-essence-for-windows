589deee166d80c9e5e7a6db1890433f6f97f844a
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-21 21:11:29 +0900

    ESModuleと即時関数

---
+++ javascript-essence-tutorial/090_モジュラーJavaScript/020_ESModuleと即時関数/start/moduleA.js
@@ -0,0 +1,11 @@
+console.log('ESmodule called')
+
+let privateVal = 1
+export let publicVal = { prop: 10 }
+
+export function publicFn() {
+  console.log('publicFn called: ' + publicVal)
+  privateval++
+}
+
+function privateFn() {}
