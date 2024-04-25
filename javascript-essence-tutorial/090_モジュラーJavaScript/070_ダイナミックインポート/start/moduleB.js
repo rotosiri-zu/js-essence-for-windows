691f0cdb562f17002bde57650aef9117da5da988
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-25 21:32:17 +0900

    ダイナミックインポート

---
+++ javascript-essence-tutorial/090_モジュラーJavaScript/070_ダイナミックインポート/start/moduleB.js
@@ -0,0 +1,13 @@
+// import { publicval, publicFn } from './moduleA.js'
+
+// publicFn()
+
+async function fn() {
+  const modules = await import('./moduleA.js')
+  modules.publicFn()
+}
+fn()
+// import('./moduleA.js').then(function (modules) {
+//   console.log(modules)
+//   modules.publicFn()
+// })
