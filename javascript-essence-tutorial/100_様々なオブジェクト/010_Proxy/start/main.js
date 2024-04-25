a4e3ac6242cb619d555e251a5615020113a5cf04
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-25 23:03:53 +0900

    Proxy

--- javascript-essence-tutorial/100_様々なオブジェクト/010_Proxy/start/main.js
+++ javascript-essence-tutorial/100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,27 @@
-const targetObj = { a: 0 };
+const targetObj = { a: 0 }
+const handler = {
+  set: function (target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`)
+    // target[prop] = value
+    throw new Error('cannot add prop')
+  },
+  set: function (target, prop, receiver) {
+    console.log(receiver)
+    if (target.hasOwnProperty(prop)) {
+      return target[prop]
+    } else {
+      return '-1'
+    }
+    console.log(`[set]: ${prop}`)
+    return (target[prop] = value)
+  },
+  deleteProperty: function (target, prop) {
+    console.log(`[set]: ${prop}`)
+    delete target[prop]
+  },
+}
+const pxy = new Proxy(targetObj, handler)
+// pxy.a = 1
+console.log(pxy.b)
+pxy.b
+delete pxy.a
