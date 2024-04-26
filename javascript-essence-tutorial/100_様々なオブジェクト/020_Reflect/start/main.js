b9618ade607eb40ba1d2c9820eb367ec63198b1d
rotosiri-zu <hongjitufang25@gmail.com>
2024-04-26 18:04:49 +0900

    Reflect

--- javascript-essence-tutorial/100_様々なオブジェクト/020_Reflect/start/main.js
+++ javascript-essence-tutorial/100_様々なオブジェクト/020_Reflect/start/main.js
@@ -1,24 +1,40 @@
 class C {
   constructor(a, b) {
-    this.a = a;
-    this.b = b;
+    this.a = a
+    this.b = b
   }
 }
 
+const obj1 = new C(1, 2)
+console.log(obj1)
+const obj2 = Reflect.construct(C, [1, 2])
+console.log(obj2)
 
-// const bob = {
-//   name: 'Bob',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   }
+console.log('c' in obj1)
+console.log(Reflect.has(obj1, 'b'))
+
+// if(Reflect.defineProperty) {
+
+// } else {
+
 // }
 
-// const tom = {
-//   name: 'Tom',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   },
-//   get hello() {
-//     return this._hello();
-//   },
-// }
+const bob = {
+  name: 'Bob',
+  _hello: function () {
+    console.log(`hello ${this.name}`)
+  },
+}
+
+const tom = {
+  name: 'Tom',
+  _hello: function () {
+    console.log(`hello ${this.name}`)
+  },
+  get hello() {
+    console.log(this)
+    return this._hello()
+  },
+}
+tom.hello
+Reflect.get(tom, 'hello')
