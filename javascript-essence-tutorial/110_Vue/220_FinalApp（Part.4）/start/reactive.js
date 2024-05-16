b700c620940fcd548e43b29f7d12efe59efef55f
rotosiri-zu <hongjitufang25@gmail.com>
2024-05-16 17:23:01 +0900

    FinalApp (Part.4)

--- javascript-essence-tutorial/110_Vue/220_FinalApp（Part.4）/start/reactive.js
+++ javascript-essence-tutorial/110_Vue/220_FinalApp（Part.4）/start/reactive.js
@@ -1,91 +1,91 @@
 const handler = {
   get(target, key, receiver) {
-    const res = Reflect.get(target, key, receiver);
-    console.log('%c[reactive:get]', 'background: green; color: white;', target, key, res);
-    track(target, key);
-    return res;
+    const res = Reflect.get(target, key, receiver)
+    console.log('%c[reactive:get]', 'background: green; color: white;', target, key, res)
+    track(target, key)
+    return res
   },
   set(target, key, value, receiver) {
-    const res = Reflect.set(target, key, value, receiver);
-    console.log('%c[reactive:set]', 'background: red; color: white;', target, key, value);
-    trigger(target, key);
-    return res;
-  }
+    const res = Reflect.set(target, key, value, receiver)
+    console.log('%c[reactive:set]', 'background: red; color: white;', target, key, value)
+    trigger(target, key)
+    return res
+  },
 }
 function reactive(target) {
-  return new Proxy(target, handler);
+  return new Proxy(target, handler)
 }
 
-let activeEffect = null;
+let activeEffect = null
 function effect(fn, { computed = false } = {}) {
   try {
-    activeEffect = fn;
-    activeEffect.computed = computed;
+    activeEffect = fn
+    activeEffect.computed = computed
     if (computed) {
-      activeEffect.dirty = true;
+      activeEffect.dirty = true
     }
-    activeEffect();
-    return activeEffect;
+    activeEffect()
+    return activeEffect
   } finally {
-    activeEffect = null;
+    activeEffect = null
   }
 }
 
-const targetMap = new WeakMap();
+const targetMap = new WeakMap()
 function track(target, key) {
   if (activeEffect === null) {
-    return;
+    return
   }
-  let depsMap = targetMap.get(target);
+  let depsMap = targetMap.get(target)
 
   if (!depsMap) {
-    targetMap.set(target, (depsMap = new Map()));
+    targetMap.set(target, (depsMap = new Map()))
   }
 
-  let deps = depsMap.get(key);
+  let deps = depsMap.get(key)
   if (!deps) {
-    depsMap.set(key, (deps = new Set()));
+    depsMap.set(key, (deps = new Set()))
   }
 
   if (!deps.has(activeEffect)) {
-    console.log('%c[effect:register]', 'background: blue; color: white;', target, key, activeEffect);
-    deps.add(activeEffect);
+    console.log('%c[effect:register]', 'background: blue; color: white;', target, key, activeEffect)
+    deps.add(activeEffect)
   }
 }
 
 function trigger(target, key) {
-  const depsMap = targetMap.get(target);
+  const depsMap = targetMap.get(target)
   if (!depsMap) {
-    return;
+    return
   }
-  const deps = depsMap.get(key);
+  const deps = depsMap.get(key)
   if (!deps) {
-    return;
+    return
   }
-  deps.forEach(effect => {
-    if(effect.computed) {
-      effect.dirty = true;
+  deps.forEach((effect) => {
+    if (effect.computed) {
+      effect.dirty = true
     } else {
-      effect();
+      effect()
     }
-  });
+  })
 }
 
-function computed(getter) {
-  let computed, value;
-  const runner = effect(getter, { computed: true });
+function computed(getter, ctx) {
+  let computed, value
+  const runner = effect(getter.bind(ctx), { computed: true })
 
   computed = {
     get value() {
       if (runner.dirty) {
-        value = runner();
-        runner.dirty = false;
-        console.log('%c[computed:refresh]', 'background: purple; color: white;', value);
+        value = runner()
+        runner.dirty = false
+        console.log('%c[computed:refresh]', 'background: purple; color: white;', value)
       }
-      
-      return value;
-    }
+
+      return value
+    },
   }
-  return computed;
+  return computed
 }
-export { effect, trigger, reactive, computed };
+export { effect, trigger, reactive, computed }
